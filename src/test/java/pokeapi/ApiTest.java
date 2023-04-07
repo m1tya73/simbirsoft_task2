3baaa76d6b176eb478d83062b87a02fe7ab43e42
DmitryKhambikov <dkhambikov@65apps.com>
2023-04-07 11:57:25 +0400

    create

---
+++ src/test/java/pokeapi/ApiTest.java
@@ -0,0 +1,19 @@
+package pokeapi;
+
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class ApiTest {
+   
+}
